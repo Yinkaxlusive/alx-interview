3e218cda6880f323492f7b02a60bf09a37f8abbe
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-08-29 14:15:55 -0700

    n

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,48 +1,59 @@
 #!/usr/bin/python3
+"""N Queens"""
 import sys
+
+
+def print_board(board, n):
+    """Print allocated positions to the queen"""
+    b = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                b.append([i, j])
+    print(b)
+
+
+def is_position_safe(board, i, j, r):
+    """Checks if the position is safe for the queen"""
+    return board[i] in (j, j - i + r, i - r + j)
+
+
+def safe_positions(board, row, n):
+    """Find all safe positions where the queen can be allocated"""
+    if row == n:
+        print_board(board, n)
+
+    else:
+        for j in range(n):
+            allowed = True
+            for i in range(row):
+                if is_position_safe(board, i, j, row):
+                    allowed = False
+            if allowed:
+                board[row] = j
+                safe_positions(board, row + 1, n)
+
+
+def create_board(size):
+    """Generates the board"""
+    return [0 * size for i in range(size)]
+
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 
 try:
-    N = int(sys.argv[1])
-except ValueError:
+    n = int(sys.argv[1])
+except BaseException:
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
-if N < 4:
+if (n < 4):
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
 
-def is_safe(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
-            return False
-    return True
-
-def solve_nqueens(N):
-    def backtrack(row, board, solutions):
-        if row == N:
-            solutions.append(board[:])
-            return
-        for col in range(N):
-            if is_safe(board, row, col):
-                board[row] = col
-                backtrack(row + 1, board, solutions)
-                board[row] = -1
-
-    solutions = []
-    board = [-1] * N
-    backtrack(0, board, solutions)
-    return solutions
-
-def print_solutions(solutions):
-    for solution in solutions:
-        print([[i, solution[i]] for i in range(len(solution))])
-
-if __name__ == "__main__":
-    solutions = solve_nqueens(N)
-    print_solutions(solutions)
+board = create_board(int(n))
+row = 0
+safe_positions(board, row, int(n))
